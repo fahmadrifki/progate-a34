aa5bcfa25b94a397d0c4de66b117eea4f1d46253
fahmadrifki <fahmadrifki@gmail.com>
2020-04-27 16:48:13 +0700

    update header, bagian utama, bagian extra 1, add beberapa class css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 /*--- Write Javascript Here ---*/
-$(document).ready(function(){
-    
-});
 
-new WOW().init();
+/*--- Slider ---*/
+var slideIndex = 0;
+showSlides();
 
-// Hamburger Mobile Menu
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function(e) {
-  $hamburger.toggleClass("is-active");
-  $('your div menu id/class').toggle();
-});
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+/*--- /Slider ---*/
